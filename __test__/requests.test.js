9ef68c21fbdd84cf1d58e8999ec43c5bca56a87c
samad <samadarshad@hotmail.co.uk>
2021-01-17 15:25:57 +0000

    using axios instead of fetch

--- __test__/requests.test.js
+++ __test__/requests.test.js
@@ -1,7 +1,8 @@
 import "regenerator-runtime/runtime";
-const fetch = require('node-fetch');
-const requestsServiceClass = require('../src/shared/requests.js');
-const request = new requestsServiceClass(fetch);
+// const fetch = require('node-fetch');
+const axios = require('axios');
+const requestsServiceClass = require('../src/shared/requests_axios.js');
+const request = new requestsServiceClass(axios);
 
 // assumed to be always online, only 50 requests per day
 const onlineTestApiEndpoint = 'https://abdus-samad-weather-journal-test.free.beeceptor.com'
