4948b17cc82f39d32a0e4a23e4ce55b11c1ec189
samad <samadarshad@hotmail.co.uk>
2021-01-17 18:33:00 +0000

    Working get form data

--- src/client/index.js
+++ src/client/index.js
@@ -13,9 +13,28 @@ import ApexCharts from 'apexcharts'
 
 import axios from 'axios';
 // const fetch = window.fetch.bind(window);
-// Client.setFetch(axios)
-let res = axios.get("https://abdus-samad-weather-journal-test.free.beeceptor.com")
-console.log("axios get", res)
+Client.setFetch(axios)
+// axios.get("https://abdus-samad-weather-journal-test.free.beeceptor.com").then(function(res) {
+//     console.log("axios get", res)
+// })
+
+// let destination_input = document.getElementById('destination-input');
+// let travelling_from_input = document.getElementById('travelling-from-input');
+// let date_input = document.getElementById('date-input');
+let entry_form = document.getElementById('entry-form');
+entry_form.addEventListener("submit", e => {
+    // Client.respondToSubmit(e, document) 
+    e.preventDefault();
+    // console.log(e.target[0].value)
+
+    let entry_input = {
+        destination_input: e.target['destination-input'].value,
+        travelling_from_input: e.target['travelling-from-input'].value,
+        date_input: e.target['date-input'].value
+    }
+    console.log(entry_input)
+    // console.log(e);
+});
 // let user_input = document.getElementsByClassName('user-input');
 // for (const element of user_input) {
 //     element.addEventListener("submit", e => Client.respondToSubmit(e, document) );
