ea0b15f654b455fb264e4cfa7d8bfd1b5b54bcae
samad <samadarshad@hotmail.co.uk>
2021-01-18 15:22:37 +0000

    working load images

--- src/client/index.js
+++ src/client/index.js
@@ -20,7 +20,31 @@ entry_form.addEventListener("submit", e => Client.respondToSubmit(e, document));
 // for (const element of user_input) {
 //     element.addEventListener("submit", e => Client.respondToSubmit(e, document) );
 // }
+// console.log("importing icon")
+// import icon from './views/weatherIcons/a01d.png'
 
+function importAll(r) {
+    return r.keys().map(r);
+}
+  
+const images = importAll(require.context('./views/weatherIcons/', false, /\.(png|jpe?g|svg)$/));
+
+// views/weatherIcons/a01d.png'
+// console.log("icon", icon)
+const frag = document.createDocumentFragment();
+for (const image of images) {
+    
+    const imageElement = document.createElement('img');
+    console.log(image)
+    imageElement.src = image;
+    frag.appendChild(imageElement);
+    // document.getElementById("results").appendChild(frag);
+}
+// const frag = document.createDocumentFragment();
+// const image = document.createElement('img');
+// image.src = icon;
+// frag.appendChild(image);
+document.getElementById("results").appendChild(frag);
 
 $('.input-group.date').datepicker({
 });
