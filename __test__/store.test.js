9076637ccc1cbf7fbc514db69fd89863a8db532d
samad <samadarshad@hotmail.co.uk>
2021-01-19 07:59:25 +0000

    store unit test

---
+++ __test__/store.test.js
@@ -0,0 +1,16 @@
+const store = require('../src/server/store.js')
+const storeUtilsClass = require('../src/server/storeUtils.js')
+const storeUtils = new storeUtilsClass(store)
+
+describe('store', () => {
+    afterEach(() => {
+        store(false)
+    });
+
+    it('when add item to store then it should show in list', done => {
+        store(1, "hello")
+        expect(store(1)).toBe("hello")
+        done()
+    })    
+
+})
